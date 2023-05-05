879a24e31448c80d86b262c32d12f85e791de7e6
tanguis <tangui.steimetz@ecole.ensicaen.fr>
2023-05-05 09:22:38 +0200

    add option to modify max allowed time for captured images

--- python/decode-image-request/app/injection_detection/metadatas_analysis/single_image_analysis.py
+++ python/decode-image-request/app/injection_detection/metadatas_analysis/single_image_analysis.py
@@ -6,5 +6,5 @@ def convert_date_str(date_str: str) -> float:
     return float(datetime.datetime.strptime(date_str, "%Y:%m:%d %H:%M:%S").timestamp())
 
 
-def single_date_consistency(date_time_sec: float) -> bool:
-    return time.time() - date_time_sec <= 60
+def single_date_consistency(date_time_sec: float, max_latency: float = 60.0) -> bool:
+    return time.time() - date_time_sec <= max_latency
